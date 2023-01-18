463e28c9defde2126fb1018252ef292bdf4bada2
Nabeel-Chhipa <nabeelchipa@gmail.com>
2023-01-18 22:17:55 +0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Typography } from '@material-ui/core'
+
+const App = () => {
+  return (
+    <Typography variant='h6'>App</Typography>
+  )
+}
+
+export default App
